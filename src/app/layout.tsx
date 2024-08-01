14062b83b6197a53a8bca55e18673ae29cc07513
Plouis53 <Plouis53@gmail.com>
2024-07-31 21:38:26 -0400

    added new comp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
+import "../styles/globals.css"; // Corrected path
 
 const inter = Inter({ subsets: ["latin"] });
 
