b15d6832b631240fd17e5e8a1d1994629eb4a4b3
Plouis53 <Plouis53@gmail.com>
2024-09-09 18:26:58 -0400

    added new imports to main

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Hero() {
-  return <div>Hero</div>;
-}
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>Welcome to Calm Play</h1>
+      <p>Your ultimate destination for mindfulness and relaxation.</p>
+    </section>
+  );
+};
 
 export default Hero;
