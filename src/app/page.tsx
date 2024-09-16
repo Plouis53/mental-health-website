355f5d0c4b9d8d4047780935fb0a64e5bf8f4e8d
Plouis53 <Plouis53@gmail.com>
2024-09-16 15:24:04 -0400

    cleaned up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,47 +4,8 @@ import Posts from "@/section/Posts";
 export default function Home() {
   return (
     <main id="main">
-      {/* <h1>Welcome to Calm Play</h1> */}
       <Hero />
       <Posts />
     </main>
   );
 }
-
-// import Hero from "@/section/Hero";
-// import Posts from "@/section/Posts";
-
-// export default function Home() {
-//   return (
-//     <main id="main">
-//       <Hero />
-//       <Posts />
-//     </main>
-//   );
-// }
-
-// export default function Home() {
-//   return (
-//     <main className="flex flex-col items-center justify-between p-24 bg-gradient-to-r from-purple-100 via-blue-100 to-gray-100">
-//       <div className="text-center max-w-5xl">
-//         <h1 className="text-4xl font-bold mb-8 text-gray-800">
-//           Welcome to Calm Play
-//         </h1>
-//         <p className="text-lg mb-4 text-gray-700">
-//           Your ultimate destination for mindfulness and relaxation through
-//           engaging and fun activities.
-//         </p>
-//         {/* Centering the Image */}
-//         <div className="relative w-full max-w-xl mx-auto mt-12 flex justify-center">
-//           <Image
-//             className="rounded-xl shadow-lg"
-//             src="/images/keep-calm.png"
-//             alt="Keep Calm"
-//             width={800}
-//             height={400}
-//             priority
-//           />
-//         </div>
-//       </div>
-//     </main>
-//   );
