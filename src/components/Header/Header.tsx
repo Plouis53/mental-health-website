19412bcfdbc28a254f65a806973c2c9fdd73676f
Plouis53 <Plouis53@gmail.com>
2024-09-16 17:43:54 -0400

    adjust the search form

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,16 @@
 import React, { useState } from "react";
 import "./header.css";
 import Nav from "../Navigation/Nav";
-import Sci from "../Social/sci";
+import Sci from "../Social/Sci";
+import SearchForm from "../SearchForm/searchForm";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const handleFormOpen = (e: Event | any) => {
+    e.preventDefault();
+    setOpen(!open);
+  };
   return (
     <header id="header" className="header d-flex align-items-center fixed-top">
       <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
@@ -15,6 +22,10 @@ export default function Header() {
         <Nav />
         <div className="position-relative"></div>
         <Sci />
+        <a className="mx-2 js-search-open" onClick={handleFormOpen}>
+          <span className="bi-search"></span>
+        </a>
+        <SearchForm active={open} formOpen={handleFormOpen} />
       </div>
     </header>
   );
---
+++ src/components/SearchForm/searchForm.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "../SearchForm/searchForm.css";
+
+export default function SearchForm({
+  active,
+  formOpen,
+}: {
+  active: boolean;
+  formOpen: Object | any;
+}) {
+  return (
+    <div
+      className={`search-form-wrap js-search-form-wrap ${
+        active ? "active" : ""
+      }`}
+    >
+      <form className="search-form">
+        <span className="icon bi-search"></span>
+        <input type="text" placeholder="Search" className="form-control" />
+        <button className="btn js-search-close" onClick={formOpen}>
+          <span className="bi-x"></span>
+        </button>
+      </form>
+    </div>
+  );
+}
+
+// import React from "react";
+// import "../SearchForm/searchForm.css";
+
+// export default function SearchForm({
+//   active,
+//   formOpen,
+// }: {
+//   active: Boolean;
+//   formOpen: Object | any;
+// }) {
+//   return (
+//     <div
+//       className={
+//         "search-form-wrap js-search-form-wrap $ {active ? 'active' :  undefined}"
+//       }
+//     >
+//       <form className="SearchForm">
+//         <span className="icon bi-search"></span>
+//         <input type="next" placeholder="search" className="form-control" />
+//         <button className="btn-js-search-close" onClick={formOpen}>
+//           <span className="bi-x"></span>
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
