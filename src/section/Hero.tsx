20f6f67bd6354de2ca0658b985c1c7c9164c5f75
Plouis53 <Plouis53@gmail.com>
2024-09-20 20:29:05 -0400

    added the useEffect

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -4,9 +4,28 @@ import React, { useEffect } from "react";
 import { heroSlides } from "@/data/data";
 // import "./hero.css";
 
-// import AOS
+// Import AOS
 import AOS from "aos";
 
+// Import Swiper React Components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// Import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
 export default function Hero() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: false,
+      mirror: false,
+    });
+  }, []);
   return <h1>Welcome to Calm Play</h1>;
 }
