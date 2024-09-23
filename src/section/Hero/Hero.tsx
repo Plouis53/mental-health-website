352b425a31b801234df39325b6975d56f1cae1bb
Plouis53 <Plouis53@gmail.com>
2024-09-23 15:26:43 -0400

    added new diminsions to swiper and added css.

--- src/section/Hero/Hero.tsx
+++ src/section/Hero/Hero.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from "react";
 import { heroSlides } from "@/data/data";
-// import "./hero.css";
+import "./hero.css";
 
 // Import AOS
 import AOS from "aos";
@@ -28,17 +28,43 @@ export default function Hero() {
       mirror: false,
     });
   }, []);
+
   return (
     <section id="hero-slider" className="hero-slider">
       <div className="container-md" data-aos="fade-in">
         <div className="row">
           <div className="col-12">
-            <Swiper>
-              {heroSlides.map((slide) => (
-                <SwiperSlide>
+            <Swiper
+              slidesPerView={"auto"}
+              speed={500}
+              autoplay={{ delay: 3000, disableOnInteraction: false }}
+              pagination={{
+                el: ".swiper-pagination",
+                type: "bullets",
+                clickable: true,
+              }}
+              navigation={{
+                nextEl: ".custom-swiper-button-next",
+                prevEl: ".custom-swiper-button-prev",
+              }}
+              modules={[Autoplay, Pagination, Navigation]}
+              loop={true}
+              className="sliderFeaturedPosts"
+            >
+              {heroSlides.map((slide, index) => (
+                <SwiperSlide key={index}>
                   <HeroSlide slide={slide} />
                 </SwiperSlide>
               ))}
+
+              <div className="custom-swiper-button-next">
+                <span className="bi-chevron-right"></span>
+              </div>
+              <div className="custom-swiper-button-prev">
+                <span className="bi-chevron-left"></span>
+              </div>
+
+              <div className="swiper-pagination"></div>
             </Swiper>
           </div>
         </div>
