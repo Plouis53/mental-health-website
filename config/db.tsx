07337cceb36f9cf9295ae6fa96ccb01567b2f771
Plouis53 <Plouis53@gmail.com>
2024-10-02 13:21:12 -0400

    added Deprecation Warnings

--- config/db.tsx
+++ config/db.tsx
@@ -2,11 +2,15 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL!);
+    await mongoose.connect(process.env.MONGO_URL!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("CONNECTED TO MONGO SUCCESS...");
-  } catch (error) {}
-  console.log("CONNECTED TO MONGO FAILED...");
-  process.exit(1);
+  } catch (error) {
+    console.error("CONNECTED TO MONGO FAILED...", error.message);
+    process.exit(1);
+  }
 };
 
 export default dbConnect;
