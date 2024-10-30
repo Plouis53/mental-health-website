cff0a24cacd566f1087b0729c74c044f6488f6dc
phillippe r louis <plouis53@gmail.com>
2024-10-30 18:20:14 -0400

    cleaned up folder and added AOS comp

--- src/app/postitems/[id]/page.tsx
+++ src/app/postitems/[id]/page.tsx
@@ -1,21 +1,22 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { initialPost, PostProps } from "@/sections/Post/Posts";
 import { useRouter } from "next/navigation";
 
 // import AOS
+import AOS from "aos";
 import SidePostItem from "@/components/SidePostItem/SidePostItem";
 import Link from "next/link";
 import Preloader from "@/components/Preloader/PreLoader";
 
 import "./style.css";
+import "../../../components/PostItemOne/postitemone.css";
 
 export default function PostItem({ params }: { params: { id: string } }) {
   const id: string = params.id;
   const router = useRouter();
-  const [item, setItem] = useState(initialPost);
-  const [items, setItems] = useState([]);
+  const [item, setItem] = useState<any | {}>({});
+  const [posts, setPosts] = useState<any | []>([]);
 
   const tabsData = [
     { id: 1, name: "Popular", active: true },
@@ -34,23 +35,41 @@ export default function PostItem({ params }: { params: { id: string } }) {
     );
   };
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: false,
+      mirror: false,
+    });
+  }, []);
+
   const getSinglePostData = () => {
     fetch(`/api/postitems/${id}`)
-      .then((res) => res.json())
-      .then((data) => setItem(data))
-      .catch((e) => console.log(e.messages));
+      .then((res) => {
+        if (res.status === 404) {
+          router.push("/not-found");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setItem(data);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
   };
 
-  const getItemsData = () => {
-    fetch("/api/postitems")
+  const getPostData = () => {
+    fetch(`/api/postitems`)
       .then((res) => res.json())
-      .then((data) => setItems(data))
+      .then((data) => setPosts(data))
       .catch((e) => console.log(e.message));
   };
 
   useEffect(() => {
     getSinglePostData();
-    getItemsData();
+    getPostData();
   }, []);
 
   const handleDeletePost = async (id: string) => {
@@ -77,8 +96,8 @@ export default function PostItem({ params }: { params: { id: string } }) {
       <section className="single-post-content">
         <div className="container">
           <div className="row">
-            <div className="col-md-9 post-content">
-              {item && item.category !== "" ? (
+            <div className="col-md-9 post-content" data-aos="fade-up">
+              {item ? (
                 <div className="single-post">
                   <div className="post-meta">
                     <span className="date">{item.category}</span>{" "}
@@ -190,13 +209,24 @@ export default function PostItem({ params }: { params: { id: string } }) {
             </div>
             <div className="col-md-3">
               <div className="aside-block">
-                <ul className="nav nav-pills custom-tab-nav mb-4">
+                <ul
+                  className="nav nav-pills custom-tab-nav mb-4"
+                  id="pills-tab"
+                  role="tablist"
+                >
                   {tabs.map((tab) => (
-                    <li className="nav-item" key={tab.id}>
+                    <li className="nav-item" role="presentation" key={tab.id}>
                       <button
                         className={`nav-link ${
                           tab.active ? "active" : undefined
                         }`}
+                        id="pills-popular-tab"
+                        data-bs-toggle="pill"
+                        data-bs-target="#pills-popular"
+                        type="button"
+                        role="tab"
+                        aria-controls="pills-popular"
+                        aria-selected="true"
                         onClick={() => handleTabActive(tab.id)}
                       >
                         {tab.name}
@@ -204,41 +234,72 @@ export default function PostItem({ params }: { params: { id: string } }) {
                     </li>
                   ))}
                 </ul>
-                <div className="tab-content">
+
+                <div className="tab-content" id="pills-tabContent">
                   <div
                     className={`tab-pane fade  ${
                       tabs[0].active ? "show active" : ""
                     }`}
+                    id="pills-popular"
+                    role="tabpanel"
+                    aria-labelledby="pills-popular-tab"
                   >
-                    {items.slice(0, 6).map((item: PostProps) => (
-                      <SidePostItem key={item._id} item={item} />
-                    ))}
+                    {posts
+                      .slice(0, 6)
+                      .map(
+                        (post: {
+                          _id: string;
+                          category: string;
+                          date: string;
+                          title: string;
+                          author: string;
+                        }) => (
+                          <SidePostItem key={post._id} item={post} />
+                        )
+                      )}
                   </div>
+
                   <div
                     className={`tab-pane fade  ${
                       tabs[1].active ? "show active" : ""
                     }`}
+                    id="pills-trending"
+                    role="tabpanel"
+                    aria-labelledby="pills-trending-tab"
                   >
-                    {items.slice(6, 12).map((item: PostProps) => (
-                      <SidePostItem key={item._id} item={item} />
-                    ))}
+                    {posts
+                      .slice(6, 12)
+                      .map(
+                        (post: {
+                          _id: string;
+                          category: string;
+                          date: string;
+                          title: string;
+                          author: string;
+                        }) => (
+                          <SidePostItem key={post._id} item={post} />
+                        )
+                      )}
                   </div>
                 </div>
               </div>
-              <h3 className="aside-title">Video</h3>
-              <div className="video-post">
-                <a
-                  target="_blank"
-                  href="https://www.youtube.com/watch?v=uHNS_ZhI62c"
-                  className="link-video"
-                >
-                  <span className="bi-play-fill"></span>
-                  <img
-                    src="/assets/img/post-landscape-3.jpg"
-                    alt=""
-                    className="img-fluid"
-                  />
-                </a>
+
+              <div className="aside-block">
+                <h3 className="aside-title">Video</h3>
+                <div className="video-post">
+                  <a
+                    target="_blank"
+                    href="https://www.youtube.com/watch?v=uHNS_ZhI62c"
+                    className="link-video"
+                  >
+                    <span className="bi-play-fill"></span>
+                    <img
+                      src="/assets/img/post-landscape-3.jpg"
+                      alt=""
+                      className="img-fluid"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
